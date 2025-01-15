b547a8928de50cdbea51fe25223c84a0e2cc2aae
Paul Oliver <puzza007@gmail.com>
2025-01-15 14:47:50 +1300

    fix: update GitHub link in mix.exs

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Enquirer.Mixfile do
     [
       maintainers: ["Martin Pretorius"],
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/glasnoster/enquirer"}
+      links: %{"GitHub" => "https://github.com/NarrativeApp/enquirer"}
     ]
   end
 
